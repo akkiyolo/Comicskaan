658acf02e329e35e1cd6c383561e59d017746811
akkiyolo <akshatshukla069@gmail.com>
2025-09-12 23:44:20 +0530

    update

---
+++ constants.ts
@@ -0,0 +1,5 @@
+export const APPWRITE_CONFIG = {
+  endpoint: 'https://cloud.appwrite.io/v1', // Replace with your Appwrite endpoint
+  projectId: '68c457dd00313b94f0a8', // Your project ID from appwrite.json
+  functionId: 'generate-comic' // Your function ID
+};
